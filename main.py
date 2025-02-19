6fcc452561f72bf7ce55e938a5d9e3f3d2389a29
trike33 <137085374+trike33@users.noreply.github.com>
2025-02-19 16:45:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import os
 from modules.hawlucha import *
 from modules.treecko import *
 from modules.gengar import *
+from modules.helper import *
 
 def goback(count):
 	count = count + 1
@@ -39,6 +40,9 @@ def signal_handler(sig, frame):
     rec_timer_event.set()
     sys.exit(0)
 
+def clear_screen():
+	"""Helper function used to clear the screen"""
+	os.system("clear") #change it to: "cls" if you are on windows
 
 if __name__ == '__main__':
 	#Registering the signal handler
@@ -50,6 +54,7 @@ if __name__ == '__main__':
 	count = 0 #variable used to only ask things the first time
 	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #disable unsecure HTTPS warning
 	while True:
+		clear_screen()
 		banner = (f"""
 
 {Fore.RED}██╗    ██╗███████╗██████╗ {Fore.WHITE}██████╗  ██████╗ ██╗  ██╗
@@ -72,14 +77,16 @@ if __name__ == '__main__':
 					os.remove('master.json')
 				else:
 					count = goback(count)
-		print("1. Search info disclosures from a file containing URLs")
-		print("2. Enter web surface discovery mode")
-		print("3. Enter hidden web discovery mode")
-		print("4. Print the tree view of the target/targets URL(usefull for small sites)")
-		print("5. Save the results into master.json")
-		print("6. Check for uniqueness in the master.json file(useful when master.json is not empty)")
-		print("7. Smartly view the results")
-		print("8. Exit")
+		print(f"{Fore.RED}\n┌" + "─" * 91 + "┐")
+		print(f"{Style.BRIGHT}| "+"1. Search info disclosures from a file containing URLs"+" "*(90-54)+"|")
+		print(f"{Style.BRIGHT}| "+"2. Enter web surface discovery mode"+" "*(90-35)+"|")
+		print(f"{Style.BRIGHT}| "+"3. Enter hidden web discovery mode"+" "*(90-34)+"|")
+		print(f"{Style.BRIGHT}| "+"4. Print the tree view of the target/targets URL(usefull for small sites)"+" "*(90-73)+"|")
+		print(f"{Style.BRIGHT}| "+"5. Save the results into master.json"+" "*(90-36)+"|")
+		print(f"{Style.BRIGHT}| "+"6. Check for uniqueness in the master.json file(useful when master.json is not empty)"+" "*(90-85)+"|")
+		print(f"{Style.BRIGHT}| "+"7. Smartly view the results"+" "*(90-27)+"|")
+		print(f"{Style.BRIGHT}| "+"8. Exit"+" "*(90-7)+"|")
+		print(f"{Fore.RED}└" + "─" * 91 + "┘"+"\n")
 		if len(unique_urls) != 0:
 			print(f"{Fore.GREEN}Results sucessfully saved!")
 		if count <= 1:
@@ -140,15 +147,18 @@ if __name__ == '__main__':
 		f"{Fore.YELLOW}   Mode 1: Sensitive Information Discovery | Development\n\n"
 		f"{Fore.GREEN}  Searching for credentials, tokens, and secrets...\n"
 	)
+			clear_screen()
 			print(banner)
 			print("Entering mode 1...")
 			inputfile = input("Please enter the file to read the URLs from or enter to load master.json urls: ")
 			if not inputfile:
 				inputfile = "master.json"
-			print("1. Priority level 1: Checks for comments and keywords on live URLs | not recommended for large sites")
-			print("2. Priority level 2: Checks for uncommon extensions on live URLs")
-			print("3. Performs the 2 priority levels")
-			print("4. Go back\n")
+			print(f"{Fore.CYAN}\n┌" + "─" * 91 + "┐")
+			print(f"{Style.BRIGHT}| 1. Priority level 1: Checks for comments and keywords on live URLs(slower)"+" "*(90-74)+"|")
+			print(f"{Style.BRIGHT}| 2. Priority level 2: Checks for uncommon extensions on live URLs"+" "*(90-64)+"|")
+			print(f"{Style.BRIGHT}| 3. Performs the 2 priority levels"+" "*(90-33)+"|")
+			print(f"{Style.BRIGHT}| 4. Go back"+" "*(90-10)+"|")
+			print(f"{Fore.CYAN}└" + "─" * 91 + "┘"+"\n")
 			print(f"{Fore.YELLOW}To save the scan results go back to the main menu and save them\n")
 			priority_level = input("Please enter the priority level(1,2,3,4): ")
 			if priority_level == "4":
@@ -181,6 +191,7 @@ if __name__ == '__main__':
      {Fore.YELLOW}   Mode 2: Web Surface Explorer | Development\n
 	 {Fore.GREEN}  Looking for developer intended public endpoints(or not so intented)\n
 """)
+			clear_screen()
 			print(banner)
 			if len(scope_urls) == 0:
 				print(f"{Fore.RED}A scope must be entered in order to continue")
@@ -196,15 +207,16 @@ if __name__ == '__main__':
 					except Exception:
 						print(f"{Fore.RED}You must provide a scope")
 						traceback.print_exc()
+						sys.exit(1)
 				else:
 					scope_urls.add(scope)
-			print("1. Check robots and sitemaps(recursively)")
-			print("2. Basic HTML/Javascript crawl")
-			print("3. Advanced HTML/Javascript crawl(headless browser)")
-			print("4. All(not implemented yet)")
-			print("5. Go back")
+			print(f"{Fore.MAGENTA}\n┌" + "─" * 91 + "┐")
+			print(f"{Style.BRIGHT}| 1. Check robots and sitemaps(recursively)"+" "*(90-41)+"|")
+			print(f"{Style.BRIGHT}| 2. Basic HTML/Javascript crawl(without browser)"+" "*(90-47)+"|")
+			print(f"{Style.BRIGHT}| 3. Go back"+" "*(90-10)+"|")
+			print(f"{Fore.MAGENTA}└" + "─" * 91 + "┘"+"\n")
 			results = set()
-			second_choice = input("Please enter the mode you would like to use(1,2,3,4,5,6):")
+			second_choice = input("Please enter the mode you would like to use(1,2,3):")
 
 			if second_choice == "1":
 				input2 = input("Please enter the robots/sitemap URL or filename(with robots/sitemap URLs): ")
@@ -226,8 +238,11 @@ if __name__ == '__main__':
 					results = parse_robots_url(input2, thread_num, results)
 					print(f"{Fore.RED}No more robots/sitemap files found")
 					print(f"{Fore.GREEN}Found {len(results)} valid URLs")
-					unique_urls.add(results)
+					for i in results:
+						unique_urls.add(i)
 					print(f"{Fore.GREEN}URLs sucessfully saved!")
+					input("Awaiting to return to main menu...")
+					
 			elif second_choice == "2":
 				url = input(f"{Fore.YELLOW}Please enter the root URL or the file containing the root URLs of the site you want to analyze: ")
 				max_depth = input(f"{Fore.YELLOW}Please enter the maxiumum depth level of recursion or press enter to leave empty: ")
@@ -259,8 +274,9 @@ if __name__ == '__main__':
 				result = pidgey_recurse(start_urls, scope_urls, int(max_depth), header, int(timeout), int(max_req_num), skip)
 				for i in result:
 					unique_urls.update(i)
+				input("")
 			
-			elif second_choice == "6":
+			elif second_choice == "3":
 				count = goback(count)
 			else:
 				print(f"{Fore.RED}Please enter a valid option!")
@@ -279,10 +295,16 @@ if __name__ == '__main__':
     Looking in the shadows...                                                                                                               
 
 """)
+			clear_screen()
 			print(banner)
-			print("1. Recursive fuzzer(only 1 URL at a time)")
-			print("2. Fetch known URLs from internet archives")
-			print("3. Go back")
+			print(f"{Fore.MAGENTA}\n┌" + "─" * 91 + "┐")
+			print(f"{Style.BRIGHT}| 1. Recursive fuzzer(only 1 URL at a time)"+" "*(90-41)+"|")
+			print(f"{Style.BRIGHT}| 2. Fetch known URLs from internet archives"+" "*(90-42)+"|")
+			print(f"{Style.BRIGHT}| 3. Find hidden input fields in HTML"+" "*(90-35)+"|")
+			print(f"{Style.BRIGHT}| 4. Brute force GET parameters(1 param and 1 URL for now)"+" "*(90-56)+"|")
+			print(f"{Style.BRIGHT}| 5. Brute force POST parameters(1 parm and 1 URL for now and only url-encoded data)"+" "*(90-82)+"|")
+			print(f"{Style.BRIGHT}| 6. Go back"+" "*(90-10)+"|")
+			print(f"{Fore.MAGENTA}└" + "─" * 91 + "┘"+"\n")
 			gengar_input = input("Please enter the input you would like to use: ")
 			if gengar_input == "1":
 				gengar_url = input("Please enter the base URL or enter to use the scope: ")
@@ -313,8 +335,10 @@ if __name__ == '__main__':
 				if not gengar_timeout:
 					gengar_timeout = 10
 				print("Example -> Cookie: PHPSession=1234567890")
-				gengar_headers = input("Please enter the custom header you would like to use: ")
-				rec_fuzz(gengar_url, gengar_wd, gengar_ext, gengar_max_req_num, gengar_timeout, gengar_headers, thread_num)			
+				gengar_headers = input("Please enter the custom header you would like to use(if any): ")
+				gengar_match = matcher()
+				gengar_filter = filtering_input()
+				rec_fuzz(gengar_url, gengar_wd, gengar_ext, int(gengar_max_req_num), int(gengar_timeout), gengar_headers, thread_num, gengar_match, gengar_filter)			
 			
 			elif gengar_input == "2":
 				gengar_host = input("Please enter the host you would like to search for or enter to use the scope: ")
@@ -323,9 +347,73 @@ if __name__ == '__main__':
 				else:
 					for gengar_url in scope_urls:
 						gau(gengar_url)
-
 			elif gengar_input == "3":
+				gengar_urls = set()
+				gengar_option = input("Please enter the URL or filename containg URLs that you want to check: ")
+				print("Example -> Cookie: PHPSession=1234567890")
+				gengar_header = input("Please enter a custom header(if any): ")
+				if "://" not in gengar_option:
+					with open(gengar_option, 'r') as file:
+						lines = file.readlines()
+						for line in lines:
+							line = line.rstrip()
+							gengar_urls.add(line)
+				else:
+					gengar_urls.add(gengar_option)
+
+				find_hidden_fields(gengar_urls, gengar_header)
+
+			elif gengar_input == "4":
+				gengar_url = input("Please enter the base URL or enter to use the scope: ")
+				if not gengar_url:
+					if not len(scope_urls) > 1:
+						gengar_url = list(scope_urls)[0]
+					else:
+						print(f"{Fore.RED} The scope contains more than 1 URL!")
+						count = goback()
+				if gengar_url[-1] == "/":
+					gengar_url = f"{gengar_url}/"
+
+				gengar_wd = input("Please enter the full path of the wordlist you would like to use: ")
+				gengar_max_req_num = input("Please enter the maxiumum paralel requests number(default 1): ")
+				if not gengar_max_req_num:
+					gengar_max_req_num = 1
+				print("Example -> Cookie: PHPSession=1234567890")
+				gengar_headers = input("Please enter the custom header you would like to use(if any): ")
+				gengar_timeout = input("Please enter the timeout for the requests in seconds (default 10s): ")
+				gengar_testvalue = input("Please enter the test value you want to use or enter(default is test_value): ")
+				if not gengar_timeout:
+					gengar_timeout = 10
+
+				bruteforce_get_params(gengar_url, gengar_wd, gengar_max_req_num, gengar_headers, gengar_timeout, gengar_testvalue)
+			elif gengar_input == "5":
+				gengar_url = input("Please enter the base URL or enter to use the scope: ")
+				if not gengar_url:
+					if not len(scope_urls) > 1:
+						gengar_url = list(scope_urls)[0]
+					else:
+						print(f"{Fore.RED} The scope contains more than 1 URL!")
+						count = goback()
+				if gengar_url[-1] == "/":
+					gengar_url = f"{gengar_url}/"
+
+				gengar_wd = input("Please enter the full path of the wordlist you would like to use: ")
+				gengar_max_req_num = input("Please enter the maxiumum paralel requests number(default 1): ")
+				if not gengar_max_req_num:
+					gengar_max_req_num = 1
+				print("Example -> Cookie: PHPSession=1234567890")
+				gengar_headers = input("Please enter the custom header you would like to use(if any): ")
+				gengar_timeout = input("Please enter the timeout for the requests in seconds (default 10s): ")
+				gengar_testvalue = input("Please enter the test value you want to use or enter(default is test_value): ")
+				if not gengar_timeout:
+					gengar_timeout = 10
+
+				bruteforce_post_params(gengar_url, gengar_wd, gengar_max_req_num, gengar_headers, gengar_timeout, gengar_testvalue)
+			elif gengar_input == "6":
 				count = goback(count)
+			else:
+				print(f"{Fore.RED}Please enter a valid choice for gengar!")
+				goback(count)
 
 		elif int(main_choice) == 4:
 			banner = (f"""{Fore.GREEN}
@@ -340,9 +428,9 @@ if __name__ == '__main__':
     Mode 4: Display saved URLs in a tree-view mode | Development
     Overviewing the targets...                                                                                            
 """)
-
+			clear_screen()
 			print(banner)
-			input("Press enter to continue: ")
+			input("Press enter to load master.json: ")
 			try:
 				initialize_tkinter_treecko('master.json')
 			except Exception:
@@ -363,14 +451,21 @@ if __name__ == '__main__':
 	Keeping things organized...                                                                                                       
 
 """)
+			clear_screen()
 			print(banner)
 			input("Press enter to continue: ")
 			#write the results to a json file
-			write_to_json(unique_urls, 'master.json')
-			print(f"{Fore.GREEN}Results sucessfully written to {os.getcwd()}/master.json")
-			unique_urls.clear()
-			print(f"{Fore.GREEN}URLs in memory flushed sucessfully!")
-
+			if len(unique_urls) != 0:
+				write_to_json(unique_urls, 'master.json')
+				print(f"{Fore.GREEN}Results sucessfully written to {os.getcwd()}/master.json")
+				unique_urls.clear()
+				print(f"{Fore.GREEN}URLs in memory flushed sucessfully!")
+				input("Awaiting to return to main...")
+			else:
+				print(f"{Fore.YELLOW}No unique urls in memory!")
+				input("Awaiting to return to main...")
+				goback(count)
+				
 		elif int(main_choice) == 6:
 			banner = (f"""{Fore.MAGENTA}
 
@@ -384,6 +479,7 @@ if __name__ == '__main__':
 	Mode 6: Looking for duplicates in the master.json file  | Development
 	Looking for twins...
 """)
+			clear_screen()
 			print(banner)
 			input(f"{Fore.MAGENTA}Press enter to continue: ")
 			try:
@@ -402,6 +498,7 @@ if __name__ == '__main__':
 						continue
 				else:
 					print(f"{Fore.MAGENTA}None duplicates were found!")
+					input(f"{Fore.MAGENTA}Press enter to continue to main menu...")
 					continue
 			except Exception:
 				print(f"{Fore.RED}There was an error while looking for or deleting duplicates")
@@ -421,54 +518,18 @@ if __name__ == '__main__':
     An Unmatched Vision for Strategic Mastery                                                                                                    
 
 """)
+			clear_screen()
 			print(banner)
-			print("1. Extension and keywords check on URLs")
-			print("2. Find guessable parameters in URLs")
-			print("3. Parse a Burp-comptabile JSON scope file")
-			print("4. Go back")
+			print(f"{Fore.RED}\n┌" + "─" * 91 + "┐")
+			print(f"{Style.BRIGHT}| 1. Extension and keywords check on URLs"+" "*(90-41+2)+"|")
+			print(f"{Style.BRIGHT}| 2. Find guessable parameters in URLs(numeric)"+" "*(90-38+2)+"|")
+			print(f"{Style.BRIGHT}| 3. Parse a Burp-comptabile JSON scope file"+" "*(90-44+2)+"|")
+			print(f"{Style.BRIGHT}| 4. Search for CVEs on the snyk website(free), by parsing HTML code"+" "*(90-68+2)+"|")
+			print(f"{Style.BRIGHT}| 5. Go back"+" "*(90-12+2)+"|")
+			print(f"{Fore.RED}└" + "─" * 91 + "┘"+"\n")
 			hawlucha_input = input("Please enter the option you would like to use: ")
-			inputfile = input("Please enter the file containing the results to analyze, or press enter to analyze all the saved results file, or m for master.json:  ")
-			urls_to_process = set()
-			urls_to_process.clear()
-			if hawlucha_input != 3:
-				if not inputfile:
-					inputfiles = ['snorlax_result.txt', 'pidgey_results.txt', 'pidgey_results_sensitive.txt']
-					for file in inputfiles:
-						if os.path.exists(file):
-							try:
-								with open(file, 'r') as file:
-									lines = file.readlines()
-									for line in lines:
-										line = line.rstrip()
-										urls_to_process.add(line)
-							except:
-								print_exc.traceback()
-						else:
-							print(f"{Fore.RED}File {file} not found!")
-				elif inputfile == "m":
-					try:
-						with open("master.json", 'r') as file:
-							urls = json.load(file)
-							for url in urls:
-								url = url.rstrip()
-								urls_to_process.add(url)
-					except:
-						traceback.print_exc()
-
-				else:
-					if os.path.exists(inputfile):
-						try:
-							with open(inputfile, 'r') as file:
-								lines = file.readlines()
-								for line in lines:
-									line = line.rstrip()
-									urls_to_process.add(line)
-						except:
-							traceback.print_exc()
-					else:
-						print(f"{Fore.RED}File {file} not found!")
-
 			if hawlucha_input == "1":
+				urls_to_process = hawlucha_file()
 				ext_keyword_check(urls_to_process)
 
 			elif hawlucha_input == "2":
@@ -479,6 +540,9 @@ if __name__ == '__main__':
 				scope_view(jsonfile)
 
 			elif hawlucha_input == "4":
+				snyk_check()
+
+			elif hawlucha_input == "5":
 				count = goback(count)
 			else:
 				print(f"{Fore.RED}You must enter a valid option!")
@@ -497,6 +561,7 @@ if __name__ == '__main__':
  	   WE HOPE TO SEE YOU AGAIN! """
 
     		)
+			clear_screen()
 			print(banner)
 			sys.exit(0)
 		else:
